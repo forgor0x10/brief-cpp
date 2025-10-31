13aef93851f380a9e00a8c39051b2f366fc7fa7f
forgor0x10 <burdigindanil@gmail.com>
2025-11-01 00:25:51 +0500

    feat: implement Cin class in io.hpp

--- include/brief/io.hpp
+++ include/brief/io.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "result.hpp"
+
 template <typename... Args> inline void print(Args... args) { ((std::cout << args), ...); }
 
 inline void println() { std::cout << "\n"; }
@@ -17,3 +19,22 @@ inline void eprintln() { std::cerr << "\n"; }
 template <typename... Args> inline void eprintln(Args... args) {
     ((std::cerr << args), ...) << "\n";
 }
+
+namespace brief::io {
+class Cin {
+  public:
+    auto read_line(std::string *str) const -> Result<unsigned long long, std::string> {
+        (void)this;
+
+        if (!std::getline(std::cin, *str)) {
+            if (std::cin.bad()) { return Err(std::string("I/O failure (badbit)")); }
+            if (std::cin.fail()) { return Err(std::string("Extraction error (failbit)")); }
+            if (std::cin.eof()) { return Err(std::string("End of file error (eofbit)")); }
+        }
+
+        return Ok((*str).length());
+    };
+};
+
+inline auto cin() -> Cin { return {}; }
+} // namespace brief::io
